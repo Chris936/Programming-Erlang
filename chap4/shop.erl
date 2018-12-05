fbf5ee6bd8471a3878503616610ed9274ede7096
Chris936 <christoffer.samuelsson222@gmail.com>
2018-12-05 19:40:40 +0100

    Simple shop module 1.

---
+++ chap4/shop.erl
@@ -0,0 +1,20 @@
+-module(shop).
+-export([run/0]).
+
+run()->
+    ThingsToBuy = [{bacon,3}, {eggs, 12}, {beer, 4}],
+    total(ThingsToBuy).
+
+cost(bacon) ->
+    10;
+cost(eggs) ->
+    2;
+cost(beer) ->
+    12.
+
+total([H|T])->
+    {What, N} = H,
+    CostN =     cost(What) * N,
+    CostN + total(T);
+total([])->
+    0.
